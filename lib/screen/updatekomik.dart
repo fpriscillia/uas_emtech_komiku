47696d72fc298751d934e2155ca27cee4d3de3ad
TrevinTerrence <trevintimisela@gmail.com>
2024-12-31 18:48:38 +0700

    update konten

--- lib/screen/updatekomik.dart
+++ lib/screen/updatekomik.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:uas_emtech_comic/class/content.dart';
 import 'package:uas_emtech_comic/class/kategoriKomik.dart';
 
 class UpdateKomik extends StatefulWidget {
@@ -28,13 +29,70 @@ class _UpdateComicState extends State<UpdateKomik> {
   bool _isLoading = false;
   List<KategoriKomik> listKategori = [];
   final ImagePicker _picker = ImagePicker();
+  List<Content> _comicPages = [];
+  String? _errorMessage;
+  final List<File> _selectedContent = [];
+  File? _selectedUpdateContent;
+
+
 
   @override
   void initState() {
     super.initState();
     _loadComicData();
     bacaKategori();
+    getContentList(widget.comicId);
   }
+
+  Future<void> getContentList(int comicId) async {
+    try {
+      final response = await http.post(
+        Uri.parse("https://ubaya.xyz/flutter/160721022/uas_komiku/content.php"),
+        body: {'comic_id': comicId.toString()},
+      );
+
+      if (response.statusCode == 200) {
+        final jsonData = json.decode(response.body);
+        if (jsonData['result'] == 'success') {
+          setState(() {
+            _comicPages = (jsonData['data'] as List)
+                .map((item) => Content.fromJson(item))
+                .toList();
+            _isLoading = false;
+          });
+        } else {
+          setState(() {
+            _errorMessage = jsonData['message'];
+            _isLoading = false;
+          });
+        }
+      } else {
+        throw Exception('Failed to load comic pages');
+      }
+    } catch (e) {
+      setState(() {
+        _errorMessage = e.toString();
+        _isLoading = false;
+      });
+    }
+  }
+  Future<void> _pickContent() async {
+    final pickedFiles = await _picker.pickMultiImage();
+    if (pickedFiles != null) {
+      setState(() {
+        _selectedContent.addAll(pickedFiles.map((file) => File(file.path)));
+      });
+    }
+  }
+  Future<void> _pickUpdateContent() async {
+    final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      setState(() {
+        _selectedUpdateContent = File(pickedFile.path);
+      });
+    }
+  }
+
 
   Future<void> _loadComicData() async {
     setState(() {
@@ -136,6 +194,142 @@ class _UpdateComicState extends State<UpdateKomik> {
 
       final jsonResponse = json.decode(response.body);
       if (jsonResponse['result'] == 'success') {
+        if (_selectedContent.isNotEmpty) {
+          final comicId = widget.comicId;
+
+          // Kirim setiap konten yang dipilih ke server
+          for (var content in _selectedContent) {
+            final contentBytes = await content.readAsBytes();
+            final contentBase64Image = base64Encode(contentBytes);
+
+            final contentResponse = await http.post(
+              Uri.parse("https://ubaya.xyz/flutter/160721022/uas_komiku/addcontent.php"),
+              body: {
+                'comic_id': comicId.toString(),
+                'gambar': contentBase64Image,
+              },
+            );
+
+            final contentJsonResponse = json.decode(contentResponse.body);
+
+            if (contentJsonResponse['result'] != 'success') {
+              throw Exception('Gagal menginput konten: ${contentJsonResponse['message']}');
+            }
+          }
+
+          // Jika semua proses berhasil
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Komik dan konten berhasil ditambahkan!')),
+          );
+        }
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Komik berhasil diperbarui!')),
+        );
+        Navigator.pop(context, 'update');
+      } else {
+        throw Exception(jsonResponse['message']);
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error: $e')),
+      );
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
+  Future<void> _changeContent(int id) async {
+    if (!_formKey.currentState!.validate()) {
+      return;
+    }
+
+    setState(() {
+      _isLoading = true;
+    });
+
+    try {
+      final posterBytes =
+          _selectedUpdateContent != null ? await _selectedUpdateContent!.readAsBytes() : null;
+      final posterBase64 = (posterBytes != null
+          ? base64Encode(posterBytes)
+          : (_initialPoster != null ? _initialPoster : null));
+
+      final response = await http.post(
+        Uri.parse(
+            "https://ubaya.xyz/flutter/160721022/uas_komiku/updatecontent.php"),
+        body: {
+          'comic_id': id.toString(),
+          'gambar': posterBase64,
+        },
+      );
+
+      final jsonResponse = json.decode(response.body);
+      if (jsonResponse['result'] == 'success') {
+        // for (var content in _selectedContents) {
+        //   final contentBytes = await content.readAsBytes();
+        //   final contentBase64 = base64Encode(contentBytes);
+
+        //   final contentResponse = await http.post(
+        //     Uri.parse(
+        //         "https://ubaya.xyz/flutter/160721022/uas_komiku/updatecontent.php"),
+        //     body: {
+        //       'comic_id': widget.comicId.toString(),
+        //       'gambar': contentBase64,
+        //     },
+        //   );
+
+        //   final contentJsonResponse = json.decode(contentResponse.body);
+        //   if (contentJsonResponse['result'] != 'success') {
+        //     throw Exception(
+        //         'Error updating content: ${contentJsonResponse['message']}');
+        //   }
+        // }
+
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('konten berhasil diperbarui!')),
+        );
+        initState();
+      } else {
+        throw Exception(jsonResponse['message']);
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error: $e')),
+      );
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+  Future<void> _deleteContent(int id) async {
+    if (!_formKey.currentState!.validate()) {
+      return;
+    }
+
+    setState(() {
+      _isLoading = true;
+    });
+
+    try {
+      final posterBytes =
+          _selectedPoster != null ? await _selectedPoster!.readAsBytes() : null;
+      final posterBase64 = (posterBytes != null
+          ? base64Encode(posterBytes)
+          : (_initialPoster != null ? _initialPoster : null));
+
+      final response = await http.post(
+        Uri.parse(
+            "https://ubaya.xyz/flutter/160721022/uas_komiku/deletecontent.php"),
+        body: {
+          'id': id.toString(),
+        },
+      );
+
+      final jsonResponse = json.decode(response.body);
+      if (jsonResponse['result'] == 'success') {
         // for (var content in _selectedContents) {
         //   final contentBytes = await content.readAsBytes();
         //   final contentBase64 = base64Encode(contentBytes);
@@ -159,7 +353,6 @@ class _UpdateComicState extends State<UpdateKomik> {
         ScaffoldMessenger.of(context).showSnackBar(
           const SnackBar(content: Text('Komik berhasil diperbarui!')),
         );
-        Navigator.pop(context, 'update');
       } else {
         throw Exception(jsonResponse['message']);
       }
@@ -286,9 +479,83 @@ class _UpdateComicState extends State<UpdateKomik> {
                       ),
                     ),
                     const SizedBox(height: 20),
+                    GestureDetector(
+                      onTap: _pickContent,
+                      child: Container(
+                        height: 200,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: _selectedContent.isEmpty
+                            ? const Center(child: Text('TAMBAH KONTEN'))
+                            : ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: _selectedContent.length,
+                          itemBuilder: (context, index) {
+                            return Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Image.file(
+                                _selectedContent[index],
+                                fit: BoxFit.cover,
+                                width: 100,
+                                height: 100,
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
                     ElevatedButton(
                       onPressed: _updateComic,
                       child: const Text('Update Komik'),
+                    ),
+                    ListView.builder(
+                      shrinkWrap: true,
+                      physics: NeverScrollableScrollPhysics(),
+                      itemCount: _comicPages.length,
+                      itemBuilder: (context, index) {
+                        final page = _comicPages[index];
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            children: [
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(10),
+                                child: Image.memory(
+                                  base64Decode(page.gambar!.split(',').last),
+                                  fit: BoxFit.fitWidth,
+                                ),
+                              ),
+                              const SizedBox(height: 8),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  ElevatedButton.icon(
+                                    onPressed: () async{
+                                      await _pickUpdateContent();
+                                      _changeContent(_comicPages[index].id);
+                                    },
+                                    icon: const Icon(Icons.edit),
+                                    label: const Text('Change'),
+                                    style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+                                  ),
+                                  ElevatedButton.icon(
+                                    onPressed: () {
+                                      _deleteContent(_comicPages[index].id);
+                                    },
+                                    icon: const Icon(Icons.delete),
+                                    label: const Text('Delete'),
+                                    style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        );
+                      },
                     ),
                   ],
                 ),
